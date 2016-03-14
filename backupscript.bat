0ff401856229ed96041c55595ef42a0f3e46427c
Sergio Vicente Gómez <sergio.vicente65@gmail.com>
2016-03-14 09:12:49 +0100

    Create batch script

---
+++ backupscript.bat
@@ -0,0 +1,21 @@
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set date=%%c-%%a-%%b)
+
+For /f "tokens=1-4 delims=:." %%a in ('echo %time%') do (set time=%%a%%b%%c%%d)
+
+set backupname=%1 %date% %time%.sql
+pause
+echo Running backup: %backupname%
+
+cd <route to "bin" directory on mysql server default directory>
+
+mysqldump  --routines --databases -u <userhere> -p<passwordhere> <databasenamehere> > <backupdestroutehere>"<databasenamehere>%backupname%"
+
+echo on
+echo Deleting old databases
+
+forfiles /p <databasedestroutehere> /s /m *.* /d -3 /c "cmd /c del @file : date >= 3days"
+
+echo Backup finished :D
+
+REM pause
